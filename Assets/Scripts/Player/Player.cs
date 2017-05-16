8e6a1b89b51620e53ed7cb276f89d0dde5629350
Lucas Clemente <lucas.cmb03@gmail.com>
2017-05-16 19:26:44 -0300

    +player sprites, +atk sprites, +monster sprites, *fixed attack bugs, +animator component attached to player and enemys GO

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,59 +3,141 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player: MonoBehaviour, IEntity {
-	private Rigidbody2D rb;
+
+    private Rigidbody2D rb;
 	private GameObject atkPivot;
 
 	public float health;
 	public float damage;
 	public float speed;
-	
-	public float atkCooldown;
+
+    public float atkSpriteCount;
+
+    public float nextAtkBotSprite;
+    public float nextAtkUpSprite;
+    public float nextAtkRightSprite;
+    public float nextAtkLeftSprite;
+
+    public float atkCooldown;
 
 	public float atkDuration;
 
 	private float nextAtk;
 
+    public Animator animator;
 
-
-	// Use this for initialization
-	void Start () {
-		rb = gameObject.GetComponent<Rigidbody2D>();
+    // Use this for initialization
+    void Start () {
+		rb = this.gameObject.GetComponent<Rigidbody2D>();
 		atkPivot = transform.GetChild(0).gameObject;
 		atkPivot.SetActive (false);
-	}
+        animator = GetComponent<Animator>();
+    }
 
-	void Update(){
+    void Update(){
 		if (GameStory.reading) {
 			rb.velocity = Vector2.zero;
 			return;
 		}
 
-		Movement();
+        Animation();
+        Movement();
 		Combat();
 		
 	}
 
+    void Animation()
+    {
+     
+        animator.SetFloat("attackBot", (nextAtkBotSprite - Time.time));
+        animator.SetFloat("attackUp", (nextAtkUpSprite - Time.time));
+        animator.SetFloat("attackRight", (nextAtkRightSprite - Time.time));
+        animator.SetFloat("attackLeft", (nextAtkLeftSprite - Time.time));
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            Debug.Log("Pressing W");
+            animator.SetFloat("stay", 0f);
+            animator.SetFloat("walk", 1f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Debug.Log("Pressing A");
+            animator.SetFloat("stay", 0f);
+            animator.SetFloat("walk", 7f);
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            Debug.Log("Pressing S");
+            animator.SetFloat("stay", 0f);
+            animator.SetFloat("walk", 5f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            Debug.Log("Pressing D");
+            animator.SetFloat("stay", 0f);
+            animator.SetFloat("walk", 3f);
+        }
+        else
+        {
+            animator.SetFloat("stay", 2f);
+            animator.SetFloat("walk", 0f);
+        }
+    }
+
 	void Movement(){
-		rb.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")) * speed;
-	}
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * 10;
+        }
+        else
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+        }
+    }
 
 	void Combat(){
-		if(Input.GetAxisRaw("Horizontal") > 0){
+
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("walk_right"))
+        {
 			atkPivot.transform.eulerAngles = new Vector3(0,0,270);
-		}else if(Input.GetAxisRaw("Horizontal") < 0){
+		}if(animator.GetCurrentAnimatorStateInfo(0).IsName("walk_left"))
+        {
 			atkPivot.transform.eulerAngles = new Vector3(0,0,90);
-		}else if(Input.GetAxisRaw("Vertical") > 0){
+		}if(animator.GetCurrentAnimatorStateInfo(0).IsName("walk_upper"))
+        {
 			atkPivot.transform.eulerAngles = new Vector3(0,0,0);
-		}else if(Input.GetAxisRaw("Vertical") < 0){
+		}if(animator.GetCurrentAnimatorStateInfo(0).IsName("walk_bottom"))
+        {
 			atkPivot.transform.eulerAngles = new Vector3(0,0,180);
 		}
 
+
 		if (Time.time > nextAtk && Input.GetButton("Fire1")) {
-			nextAtk = Time.time + atkCooldown;
+            nextAtk = Time.time + atkCooldown;
 			StartCoroutine(Attack());
-		}
-	}
+            if (Time.time > atkSpriteCount && atkPivot.transform.eulerAngles.z == 180)
+            {
+                nextAtkBotSprite = Time.time + atkSpriteCount;
+                animator.SetFloat("attackBot", nextAtkBotSprite);
+            }
+            else if (Time.time > atkSpriteCount && atkPivot.transform.eulerAngles.z == 0)
+            {
+                nextAtkUpSprite = Time.time + atkSpriteCount;
+                animator.SetFloat("attackUp", nextAtkUpSprite);
+            }
+            else if (Time.time > atkSpriteCount && atkPivot.transform.eulerAngles.z == 270)
+            {
+                nextAtkRightSprite = Time.time + atkSpriteCount;
+                animator.SetFloat("attackRight", nextAtkRightSprite);
+            }
+            else if (Time.time > atkSpriteCount && atkPivot.transform.eulerAngles.z == 90)
+            {
+                nextAtkLeftSprite = Time.time + atkSpriteCount;
+                animator.SetFloat("attackLeft", nextAtkLeftSprite);
+            }
+        }
+    }
 
 	IEnumerator Attack(){
 		atkPivot.SetActive(true);
